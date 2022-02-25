7cc8dd19d6e6d84b672783501be8f29a7ac6f06e
Metalit <kylesmithhanna@gmail.com>
2022-02-25 11:12:20 -0600

    Wrong .so in copy script

--- copy.ps1
+++ copy.ps1
@@ -1,6 +1,6 @@
 & $PSScriptRoot/build.ps1
 if ($?) {
-    adb push build/libplaylistmanager.so /sdcard/Android/data/com.beatgames.beatsaber/files/mods/libplaylistmanager.so
+    adb push build/libbeatsaviordata.so /sdcard/Android/data/com.beatgames.beatsaber/files/mods/libbeatsaviordata.so
     if ($?) {
         adb shell am force-stop com.beatgames.beatsaber
         adb shell am start com.beatgames.beatsaber/com.unity3d.player.UnityPlayerActivity
